a3289da864b976f783f959cd5175d4fdafacc318
Daniel Yuschick <danyuschick@gmail.com>
2020-10-10 17:07:11 +0300

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-![Easy accessibility with React-Focus-On](modal.gif)
-
-This repo supplements the following Medium article: Easy Accessibility with React-Focus-On
-
----
-
-**React-Focus-On**
-
-https://github.com/theKashey/react-focus-on
---
+++ precache-manifest.c33355f40ddcd4142d8a2668955dd9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314209d05e615ceb6f8a5239144f542b",
+    "url": "/react-focus-on-demo/index.html"
+  },
+  {
+    "revision": "88b92d577150b0afeea3",
+    "url": "/react-focus-on-demo/static/js/2.b84a8e14.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-focus-on-demo/static/js/2.b84a8e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4f86252d9764401bae",
+    "url": "/react-focus-on-demo/static/js/main.08589548.chunk.js"
+  },
+  {
+    "revision": "1cb04514ba5d98bbbf03",
+    "url": "/react-focus-on-demo/static/js/runtime-main.723bfdcb.js"
+  },
+  {
+    "revision": "cdeb83f7b245e8410a7961653cf58748",
+    "url": "/react-focus-on-demo/static/media/double-bubble-outline.cdeb83f7.png"
+  }
+]);
